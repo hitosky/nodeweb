815d9fea55857f78682680dcede6a7f3652189a9
chongya <chongya@wacai.com>
2016-08-15 10:32:34 +0800

    second commit

--- server/components/render-html.js
+++ server/components/render-html.js
@@ -3,10 +3,22 @@
 */
 
 var ejs = require('ejs');
+var fs = require('fs');
+
 var route = require('../route.js');
 function renderHtml(path,data){
 	path=path.replace(/^\/*/,'/').replace(/\/*$/,'').replace(/\/+/g,'/');
-	return route[path] ?  ejs.render({url:route[path]},data):'<h1>404 not found</h1>'; 
+	var html = '<h1>404 not found</h1>';
+	if(route[path]){ 
+		try{
+			var str = fs.readFileSync(route[path],'utf8');
+			html = ejs.render(str,data);
+		}
+		catch(e){
+			console.error(e);
+		}
+	}
+	return html;
 }
 
 module.exports = {
