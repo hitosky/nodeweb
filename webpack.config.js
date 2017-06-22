eb73223494559ad21ffc8fd70db20877e2b623ff
chongya <chongya@wacai.com>
2017-06-22 11:48:50 +0800

    升级webpack配置和package.json

---
+++ webpack.config.js
@@ -0,0 +1,185 @@
+'use strict'
+
+const path = require('path')
+const glob = require('glob')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const os = require('os')
+const HappyPack = require('happypack')
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
+
+const entry = {}
+const plugins = []
+const root = __dirname;
+const srcPath = path.join(root, 'app')
+const buildPath = path.join(root, 'build')
+const env = process.env.NODE_ENV || 'development'
+
+// 入口文件
+glob.sync('{common,pages}/**/*.js', {
+  cwd: srcPath
+}).forEach((filePath) => {
+  const chunk = filePath.slice(0, -3)
+  entry[chunk] = [`./${chunk}`]
+})
+
+// 模版文件
+glob.sync('**/*.html', {
+  cwd: srcPath
+}).forEach((filePath) => {
+  const chunk = filePath.slice(0, -5)
+  const chunkFile = `${chunk}.html`
+
+  if (chunk.match(/^pages\//) && entry[chunk]) {
+    plugins.push(new HtmlWebpackPlugin({
+      filename: chunkFile,
+      template: chunkFile,
+      chunks: [
+        'common/index',
+        chunk
+      ]
+    }))
+  } else {
+    plugins.push(new HtmlWebpackPlugin({
+      inject: false,
+      filename: chunkFile,
+      template: chunkFile
+    }))
+  }
+})
+
+const config = {
+  context: srcPath,
+  entry: entry,
+  devtool: 'cheap-module-source-map',
+  output: {
+    pathinfo: true,
+    path: buildPath,
+    filename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+  resolve: {
+    alias: {
+      c: path.join(srcPath, 'components'),
+      common: path.join(srcPath, 'common'),
+      components: path.join(srcPath, 'components')
+    },
+    modules: [
+      srcPath,
+      path.join(srcPath,'components'),
+      'node_modules'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'happypack/loader',
+        options: {
+          id: 'js'
+        }
+      },
+      {
+        test: /\.tpl$/,
+        loader: 'dot-tpl-loader'
+      },
+      {
+        oneOf: [
+          {
+            test: /\.html$/,
+            resourceQuery: /\?.*/,
+            use: [
+              'nunjucks-loader',
+              'extract-loader',
+              'html-loader'
+            ]
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        oneOf: [
+          {
+            test: /\.(png|gif|jpg|jpeg|svg|woff|ttf|eot)$/,
+            resourceQuery: /\?.*/,
+            loader: 'url-loader'
+          },
+          {
+            test: /\.(png|gif|jpg|jpeg|svg|woff|ttf|eot)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(less|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'happypack/loader',
+            options: {
+              id: 'less'
+            }
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HappyPack({
+      id: 'js',
+      threadPool: happyThreadPool,
+      loaders: [ 'babel-loader' ],
+      verbose: true,
+      verboseWhenProfiling: true
+    }),
+    new HappyPack({
+      id: 'less',
+      threadPool: happyThreadPool,
+      loaders: [{
+        loader: 'css-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      //{
+        //loader: 'postcss-loader'
+      //},
+      {
+        loader: 'less-loader',
+        options: {
+          sourceMap: true
+        }
+      }],
+      verbose: true,
+      verboseWhenProfiling: true,
+      cache: false
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(env)
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common/index',
+      filename: 'common.js',
+      minChunks: 4
+    })
+  ].concat(plugins)
+}
+
+module.exports = config
